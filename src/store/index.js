29f7a02ac156bb007c32d918429431d343757422
Atakan <dev@tararoutray.com>
2024-06-14 19:48:00 +0600

    added create product function from admin

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,14 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
-
+import productsReducer from './slices/productCreateSliceFromAdmin';
 
 
 
 const reducer = combineReducers({
-    
+    products: productsReducer
 })
 
 
 
 export const store = configureStore({
-    
+    reducer
 })
